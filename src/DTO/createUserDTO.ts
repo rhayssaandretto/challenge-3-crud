fd1205e9369ade5f4a91f5af4427d16d97eab27c
Rhayssa <rha.andretto@gmail.com>
2023-12-30 18:53:19 -0300

    feat: add create user DTO

---
+++ src/DTO/createUserDTO.ts
@@ -0,0 +1,10 @@
+export interface createUserDTO {
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  city: string;
+  country: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
