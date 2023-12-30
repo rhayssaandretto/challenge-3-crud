a7db32c9e6e3faf3318bd2f2bcfd3e06a3dd0c7b
Rhayssa <rha.andretto@gmail.com>
2023-12-29 23:50:13 -0300

    feat: add authenticate middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,31 @@
+import Auth from 'services/Auth';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+export function authMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    return response
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: 'Unauthorized' });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  const tokenData = Auth.verifyToken(token);
+
+  if (tokenData === 'INVALID_TOKEN') {
+    return response
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: 'Unauthorized' });
+  }
+
+  request.headers.email = tokenData.email;
+
+  next();
+}
