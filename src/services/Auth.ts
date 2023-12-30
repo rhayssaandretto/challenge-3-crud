bb3f29ac980cec258304ba0f01dfa80e1312b73b
Rhayssa <rha.andretto@gmail.com>
2023-12-29 23:51:10 -0300

    feat: add authenticate class

---
+++ src/services/Auth.ts
@@ -0,0 +1,30 @@
+import * as jwt from 'jsonwebtoken';
+
+interface IJwtData {
+  email: string;
+}
+
+class Auth {
+  private static readonly SECRET: string | undefined = process.env.JWT_SECRET;
+
+  public static generateToken(data: IJwtData): string {
+    if (Auth.SECRET === undefined) {
+      throw new Error('JWT_SECRET is not defined.');
+    }
+    return jwt.sign(data, Auth.SECRET, { expiresIn: '3h' });
+  }
+
+  public static verifyToken(token: string): IJwtData | 'INVALID_TOKEN' {
+    if (Auth.SECRET === undefined) {
+      throw new Error('JWT_SECRET is not defined.'); //500
+    }
+    const decoded = jwt.verify(token, Auth.SECRET);
+    if (typeof decoded === 'string') {
+      return 'INVALID_TOKEN';
+    }
+
+    return decoded as IJwtData;
+  }
+}
+
+export default Auth;
