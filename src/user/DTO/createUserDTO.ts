03a1bee3395fb22869b7ab2cdf1ca5f35478bfd5
Rhayssa <rha.andretto@gmail.com>
2024-01-04 22:35:55 -0300

    feat: add zod validation

--- src/user/DTO/createUserDTO.ts
+++ src/user/DTO/createUserDTO.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export interface CreateUserDTO {
   firstName: string;
   lastName: string;
@@ -8,3 +10,14 @@ export interface CreateUserDTO {
   password: string;
   confirmPassword: string;
 }
+
+export const CreateUserSchema = z.object({
+  firstName: z.string().regex(/^[a-zA-Z]+$/),
+  lastName: z.string().regex(/^[a-zA-Z]+$/),
+  birthDate: z.date(),
+  city: z.string().regex(/^[a-zA-Z]+$/),
+  country: z.string().regex(/^[a-zA-Z]+$/),
+  email: z.string().email(),
+  password: z.string().min(6),
+  confirmPassword: z.string(),
+});
