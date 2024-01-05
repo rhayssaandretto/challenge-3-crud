0e0c886e8aa1dd584e23c577a1f1f034d0747504
Rhayssa <rha.andretto@gmail.com>
2024-01-04 22:31:11 -0300

    feat: implement remaining routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,5 +13,9 @@ const eventRoutes = Router();
 const eventController: EventController = new EventController();
 
 eventRoutes.post('/events', authMiddleware, eventController.create);
+eventRoutes.get('/events/:id', authMiddleware, eventController.getById);
+eventRoutes.get('/events', authMiddleware, eventController.getAll);
+eventRoutes.delete('/events/:id', authMiddleware, eventController.deleteById);
+eventRoutes.delete('/events', authMiddleware, eventController.deleteByWeekday);
 
 export { userRoutes, eventRoutes };
