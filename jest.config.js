a9d2e60ffe5adc38df6f5c86a7858fada44d9610
Rhayssa <rha.andretto@gmail.com>
2024-01-05 05:21:24 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  testTimeout: 10000,
+};
