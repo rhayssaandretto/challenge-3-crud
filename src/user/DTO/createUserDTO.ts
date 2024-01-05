307ecea40670a2fd12740f857cc9ca3db088e71d
Rhayssa <rha.andretto@gmail.com>
2024-01-05 05:22:50 -0300

    feat: implement new date format validation

--- src/user/DTO/createUserDTO.ts
+++ src/user/DTO/createUserDTO.ts
@@ -11,12 +11,25 @@ export interface CreateUserDTO {
   confirmPassword: string;
 }
 
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+const parseAndValidateDate = (value: unknown) => {
+  if (typeof value === 'string' && dateRegex.test(value)) {
+    return new Date(value);
+  }
+  throw new Error('Invalid date format');
+};
+
+const DateStringType = z.string().refine(parseAndValidateDate, {
+  message: 'Invalid date format',
+});
+
 export const CreateUserSchema = z.object({
   firstName: z
     .string()
     .min(5, { message: 'Must be 5 or more characters long' }),
   lastName: z.string().min(5, { message: 'Must be 5 or more characters long' }),
-  birthDate: z.date(),
+  birthDate: DateStringType,
   city: z.string(),
   country: z.string(),
   email: z.string().email({ message: 'Invalid email address' }),
